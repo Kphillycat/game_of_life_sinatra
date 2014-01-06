508e8e7fa0758ad73754b0f67bef2a483f84c9ae
Kphillycat <keithwilliamsjr@gmail.com>
2014-01-06 12:47:14 -0500

    Add scss file with proper linking to work with Sinatra

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source "https://rubygems.org"
 ruby '2.0.0'
 gem "sinatra"
+gem "sass"
+gem "compass"
 
 group :development do
   gem "shotgun"
--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ Dir.glob('./lib/*.rb') do |model|
   require model
 end
 
+
 module Gol
   class App < Sinatra::Application
     
@@ -16,6 +17,15 @@ module Gol
     configure do
       set :root, File.dirname(__FILE__)
       set :public_folder, 'public'
+
+      Compass.configuration do |config|
+        config.project_path = File.dirname(__FILE__)
+        config.sass_dir = 'views'
+      end
+
+      set :haml, { :format => :html5 }
+      set :sass, Compass.sass_engine_options
+      set :scss, Compass.sass_engine_options
     end
 
     #routes
@@ -26,12 +36,16 @@ module Gol
     get '/gol' do
       @cur_world = @@da_world      
       @@da_world.tick!
-      if @cur_world.tick_ct == 20
-        @refresh = ""
-      else
-        @refresh = "<meta http-equiv='refresh' content='0.1' >"
-      end
+      # if @cur_world.tick_ct == 20
+      #   @refresh = ""
+      # else
+      #   @refresh = "<meta http-equiv='refresh' content='0.1' >"
+      # end
       erb :gol
+    end
+
+    get '/custom.css' do
+      scss :custom
     end
 
     #helpers
